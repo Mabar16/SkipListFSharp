c4704d75902cc1d96964d48eb2305d4e3b2cfc21
Victor <victor.s@live.dk>
2020-05-04 14:34:57 +0200

    test

---
+++ TestApp1/TestApp1/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+//#r "FsCheck"
+
+open FsCheck
+open System
+open DataStructures.Lists
+
+
+
+[<EntryPoint>]
+let main argv =
+    let myList = SkipList() : SkipList<int> in 
+        myList.Add(5);
+     
+        let theresult = "HELLO"+myList.DeleteMin().ToString() + "WORLD" in
+            Console.WriteLine theresult;
+    0 // return an integer exit code
