e090fb072d11d04cb905d80370cf16cbca7ae98f
Markus <mabar16@student.sdu.dk>
2020-05-08 15:34:44 +0200

    Command counters

--- TestApp1/TestApp1/Program.fs
+++ TestApp1/TestApp1/Program.fs
@@ -5,6 +5,7 @@ open FsCheck
 open System
 open DataStructures.Lists
 open FsCheck.Experimental
+
 //module List
 let mutable statslist = [] :List<int>
 
@@ -27,12 +28,23 @@ let isSkipListEqualToList = fun (skipList:SkipList<int>) (fsList:List<int>) ->
 
 let numberGen = Gen.frequency([(8,Arb.generate<int>);(1,Gen.constant(System.Int32.MinValue));(1,Gen.constant(System.Int32.MaxValue))])
 
+let mutable addCounter = 0
+let mutable remCounter = 0
+let mutable findCounter = 0
+let mutable peekCounter = 0
+let mutable clearCounter = 0
+let mutable emptyCounter = 0
+let mutable containsCounter = 0
+let mutable countCounter = 0
+
+
 let skipListSpec = 
     let add i = 
         { new Operation<SkipList<int>,List<int>>() with
             member __.Run m = List.sort (i::m)
             member __.Check (c, m) =
-                c.Add(i)
+                addCounter <- addCounter + 1
+                c.Add(i) //|> Prop.collect(i)
                 statslist <- i::statslist
                 let res = isSkipListEqualToList c m//c.Contains(i)
                    in res = true
@@ -44,6 +56,7 @@ let skipListSpec =
             override __.Pre m = 
                 (m.Length) > 0
             member __.Check (c,m) = 
+                remCounter <- remCounter + 1
                 statslist <- i::statslist
                 c.Remove(i) |> ignore;
                 let res =isSkipListEqualToList c m 
@@ -56,6 +69,7 @@ let skipListSpec =
             member __.Run m = []
             member __.Check (c,m) = 
                 c.Clear()
+                clearCounter <- clearCounter + 1
                 let res = isSkipListEqualToList c m && c.IsEmpty
                 in res = true
                 |@ sprintf "Clear: model = %s, actual = %s" (m.ToString()) (c.ToString())
@@ -67,7 +81,7 @@ let skipListSpec =
             override __.Pre m = 
                 (m.Length) > 0
             member __.Check (c,m) = 
-                
+                peekCounter <- peekCounter + 1
                 let res = c.Peek()
                 in let mres = m.Head
                     in res = mres
@@ -79,6 +93,7 @@ let skipListSpec =
             member __.Run m = m
             member __.Check (c,m) = 
                 statslist <- i::statslist
+                findCounter <- findCounter + 1
                 let res = c.Find(i) // res is a bool * int tuple
                 in let mres = List.contains i m
                     in ((mres = false && (fst res) = false) ||                    
@@ -90,6 +105,7 @@ let skipListSpec =
           { new Operation<SkipList<int>, List<int>>() with
               member __.Run m = m
               member __.Check (c,m) = 
+                  containsCounter <- containsCounter + 1
                   statslist <- i::statslist
                   let res = c.Contains(i) // res is a bool
                   in let mres = List.contains i m
@@ -101,6 +117,7 @@ let skipListSpec =
         { new Operation<SkipList<int>, List<int>>() with
             member __.Run m = m
             member __.Check (c, m) =
+                countCounter <- countCounter + 1
                 let res = c.Count = m.Length
                 in res = true
                 |@ sprintf "Count: model = %i, actual = %i" m.Length c.Count
@@ -110,6 +127,7 @@ let skipListSpec =
         { new Operation<SkipList<int>, List<int>>() with
             member __.Run m = m
             member __.Check (c, m) =
+                emptyCounter <- emptyCounter + 1
                 let res = (c.IsEmpty = (m.Length = 0))
                 in res = true
                 |@ sprintf "IsEmpty: model = %b, actual = %b" (m.Length = 0) c.IsEmpty
@@ -134,14 +152,23 @@ let rec countelemsininterval list min max accum = match list with
     then countelemsininterval rest min max (1+accum) 
     else countelemsininterval rest min max accum
     
-
+let commandSum = addCounter + remCounter + clearCounter + countCounter + emptyCounter + containsCounter + findCounter + peekCounter
 let printstats list =  
     
-    Console.WriteLine("less than 0: " + (countelemsininterval list -999999 0 0).ToString())
-    Console.WriteLine("0-5: " + (countelemsininterval list 0 5 0).ToString())
-    Console.WriteLine("5-100: " + (countelemsininterval list 5 100 0).ToString())
-    Console.WriteLine("100-1000: " + (countelemsininterval list 100 1000 0).ToString())
-    Console.WriteLine("LARGE: " + (countelemsininterval list 1000 99999999 0).ToString())
+    Console.WriteLine("0-20: " + (countelemsininterval list 0 20 0).ToString())
+    Console.WriteLine("20-40: " + (countelemsininterval list 20 40 0).ToString())
+    Console.WriteLine("40-60: " + (countelemsininterval list 40 60 0).ToString())
+    Console.WriteLine("60-80: " + (countelemsininterval list 60 80 0).ToString())
+    Console.WriteLine("80-100: " + (countelemsininterval list 80 101 0).ToString())
+    Console.WriteLine("\n")
+    Console.WriteLine("Adds: "+ float(float(addCounter)/float(commandSum)).ToString())
+    Console.WriteLine("Rems: "+ float(float(remCounter)/float(commandSum)).ToString())
+    Console.WriteLine("Clears: "+ float(float(clearCounter)/float(commandSum)).ToString())
+    Console.WriteLine("Counts: "+ float(float(countCounter)/float(commandSum)).ToString())
+    Console.WriteLine("Empties: "+ float(float(emptyCounter)/float(commandSum)).ToString())
+    Console.WriteLine("Contains: "+ float(float(containsCounter)/float(commandSum)).ToString())
+    Console.WriteLine("Finds: "+ float(float(findCounter)/float(commandSum)).ToString())
+    Console.WriteLine("Peeks: "+ float(float(peekCounter)/float(commandSum)).ToString())
     
 printstats statslist
 let main argv =
